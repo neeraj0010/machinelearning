0edc0eda2468e3a99232a139fb77352002982e23
neeraj0010 <nkjha034@gmail.com>
2018-04-01 15:45:52 +0530

    test

---
+++ Pythonstart.py
@@ -0,0 +1,55 @@
+def madrid():
+    print("heelooo")
+
+# if __name__=="__main__":
+#  madrid()
+#
+
+
+
+# madrid=0.55
+# print(madrid)
+#
+# madrid = "test site"
+# print(madrid)
+
+# Concatinating data
+
+# madrid= 0.25
+# madrid1="Test Site Python"
+#
+# print(str(madrid)+madrid1)
+
+
+# Local and Global variable
+# madrid=100
+# print(madrid)
+#
+# def madridsol():
+#     global madrid
+#     print(madrid)
+#     madrid = "I Love madrid"
+# madridsol()
+# print(madrid)
+
+#STRINGS
+
+var1="Neeraj"
+var2="Sachin Arora"
+
+print("var1[0]:",var1[0])
+
+print("var2[0]:",var2[0:6])
+
+#Replace
+
+var1="Neeraj"
+var2="Sachin Arora"
+
+var3=var2.replace("arora","Neeraj")
+print(var3)
+
+print(var3.upper())
+print(var3.lower())
+
+print(var3.split(" "))
