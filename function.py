dbb5641ffe8989fac60123a3629e7e033e8ec76f
neeraj0010 <nkjha034@gmail.com>
2018-04-04 00:17:45 +0530

    test

--- function.py
+++ function.py
@@ -5,3 +5,18 @@ def my_func():
 x = 20
 my_func()
 print("Value outside function:",x)
+
+
+
+# Second Function Statement
+
+def changeme( mylist ):
+   "This changes a passed list into this function"
+   mylist.append([1,2,3,4]);
+   print("Values inside the function: ", mylist)
+   return
+
+# Now you can call changeme function
+mylist = [10,20,30];
+changeme( mylist );
+print("Values outside the function: ", mylist)
